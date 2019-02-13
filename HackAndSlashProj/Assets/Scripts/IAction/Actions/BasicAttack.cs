7d26c51ab8dabda583bf9b97673eb0dcd5dfabca
Soverilus <30487703+Soverilus@users.noreply.github.com>
2019-02-13 20:49:33 +0800

    j

---
+++ HackAndSlashProj/Assets/Scripts/IAction/Actions/BasicAttack.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttack : IActions {
+    public void PerformAction() {
+        Debug.Log("BasicAttack started");
+    }
+}
---
+++ HackAndSlashProj/Assets/Scripts/IAction/Actions/BasicDefend.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDefend : IActions {
+    public void PerformAction() {
+        Debug.Log("BasicDefend attempt started");
+    }
+}
