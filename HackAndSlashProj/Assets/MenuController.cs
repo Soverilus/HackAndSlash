575652f38591556f43ff3db6187acd595b9cdf4b
Soverilus <30487703+Soverilus@users.noreply.github.com>
2019-03-12 14:21:30 +0800

    Did the Main Menu stuff sorta almost

--- HackAndSlashProj/Assets/MenuController.cs
+++ HackAndSlashProj/Assets/MenuController.cs
@@ -6,11 +6,29 @@ public class MenuController : MonoBehaviour {
     public SpriteRenderer shopMenuArt;
     bool fadeToShop = false;
     bool fadeToCredits = false;
-    public Text[] MainMenuTexts;
-    public Image[] MainMenuButtonImages;
+    public Text[] mainMenuTexts;
+    public Image[] mainMenuButtonImages;
+    public Button[] mainMenuButtons;
+
+    public Text[] shopMenuTexts;
+    public Image[] shopMenuButtonImages;
+    public Button[] shopMenuButtons;
 
     private void Start() {
+        for (int i = 0; i < mainMenuTexts.Length; i++) {
+            mainMenuTexts[i].color = new Color(mainMenuTexts[i].color.r, mainMenuTexts[i].color.g, mainMenuTexts[i].color.b, 0);
+        }
+        for (int i = 0; i < mainMenuButtonImages.Length; i++) {
+            mainMenuButtonImages[i].color = new Color(mainMenuButtonImages[i].color.r, mainMenuButtonImages[i].color.g, mainMenuButtonImages[i].color.b, 0);
+        }
+        for (int i = 0; i < shopMenuTexts.Length; i++) {
+            shopMenuTexts[i].color = new Color(shopMenuTexts[i].color.r, shopMenuTexts[i].color.g, shopMenuTexts[i].color.b, 0);
+        }
+        for (int i = 0; i < shopMenuButtonImages.Length; i++) {
+            shopMenuButtonImages[i].color = new Color(shopMenuButtonImages[i].color.r, shopMenuButtonImages[i].color.g, shopMenuButtonImages[i].color.b, 0);
+        }
         shopMenuArt.color = new Color(shopMenuArt.color.r, shopMenuArt.color.g, shopMenuArt.color.b, 0);
+        FadeShopMenuElements(-1);
     }
 
     private void Update() {
@@ -37,24 +55,60 @@ public class MenuController : MonoBehaviour {
 
     void FadeToShop() {
         if (shopMenuArt.color.a < 1) {
-            shopMenuArt.color = new Color(shopMenuArt.color.r, shopMenuArt.color.g, shopMenuArt.color.b, shopMenuArt.color.a + 1f * Time.deltaTime);
             FadeMainMenuElements(-1f);
+            FadeShopMenuElements(1f);
         }
         else {
             //SetPurchaseMenutoActive
         }
     }
     void FadeToMain() {
-        if (shopMenuArt.color.a > 0) {
-            shopMenuArt.color = new Color(shopMenuArt.color.r, shopMenuArt.color.g, shopMenuArt.color.b, shopMenuArt.color.a - 1f * Time.deltaTime);
+        if (shopMenuArt.color.a > 0 || mainMenuButtonImages[0].color.a < 1) {
             FadeMainMenuElements(1f);
+            FadeShopMenuElements(-1f);
+        }
+    }
+
+    void FadeShopMenuElements(float positive) {
+        shopMenuArt.color = new Color(shopMenuArt.color.r, shopMenuArt.color.g, shopMenuArt.color.b, shopMenuArt.color.a + positive * Time.deltaTime);
+        if (positive > 0) {
+            for (int i = 0; i < shopMenuButtons.Length; i++) {
+                shopMenuButtons[i].gameObject.SetActive(true);
+            }
+        }
+        else {
+            for (int i = 0; i < shopMenuButtons.Length; i++) {
+                if (shopMenuButtonImages[i].color.a <= 0f) {
+                    shopMenuButtons[i].gameObject.SetActive(false);
+                }
+            }
+        }
+        for (int i = 0; i < shopMenuTexts.Length; i++) {
+            shopMenuTexts[i].color = new Color(shopMenuTexts[i].color.r, shopMenuTexts[i].color.g, shopMenuTexts[i].color.b, shopMenuTexts[i].color.a + positive * Time.deltaTime);
+        }
+        for (int i = 0; i < shopMenuButtonImages.Length; i++) {
+            shopMenuButtonImages[i].color = new Color(shopMenuButtonImages[i].color.r, shopMenuButtonImages[i].color.g, shopMenuButtonImages[i].color.b, shopMenuButtonImages[i].color.a + positive * Time.deltaTime);
         }
     }
 
     void FadeMainMenuElements(float positive) {
-        for (int i = 0; i < MainMenuTexts.Length; i++) {
-            MainMenuTexts[i].color = new Color(MainMenuTexts[i].color.r, MainMenuTexts[i].color.g, MainMenuTexts[i].color.b, MainMenuTexts[i].color.a + positive * Time.deltaTime);
-            MainMenuButtonImages[i].color = new Color(MainMenuButtonImages[i].color.r, MainMenuButtonImages[i].color.g, MainMenuButtonImages[i].color.b, MainMenuButtonImages[i].color.a + positive * Time.deltaTime);
+        if (positive > 0) {
+            for (int i = 0; i < mainMenuButtons.Length; i++) {
+                mainMenuButtons[i].gameObject.SetActive(true);
+            }
+        }
+        else {
+            for (int i = 0; i < mainMenuButtons.Length; i++) {
+                if (mainMenuButtonImages[i].color.a <= 0f) {
+                    mainMenuButtons[i].gameObject.SetActive(false);
+                }
+            }
+        }
+        for (int i = 0; i < mainMenuTexts.Length; i++) {
+            mainMenuTexts[i].color = new Color(mainMenuTexts[i].color.r, mainMenuTexts[i].color.g, mainMenuTexts[i].color.b, mainMenuTexts[i].color.a + positive * Time.deltaTime);
+        }
+        for (int i = 0; i < mainMenuButtonImages.Length; i++) {
+            mainMenuButtonImages[i].color = new Color(mainMenuButtonImages[i].color.r, mainMenuButtonImages[i].color.g, mainMenuButtonImages[i].color.b, mainMenuButtonImages[i].color.a + positive * Time.deltaTime);
         }
     }
 }
