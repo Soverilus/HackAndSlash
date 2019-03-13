0681f10287ab90fb0867595c4d6af08bdd81ce2f
Soverilus <30487703+Soverilus@users.noreply.github.com>
2019-03-13 13:41:32 +0800

    Update GameLoopController.cs

--- HackAndSlashProj/Assets/Scripts/GameLoopController.cs
+++ HackAndSlashProj/Assets/Scripts/GameLoopController.cs
@@ -6,5 +6,24 @@ public class GameLoopController : MonoBehaviour
 {
 //Load health between scenes - save current health as PlayerPref?
 //Score keeping - currency and gold prize for winning
+        //Maybe have a playerprefs controller?
+        //User settings script?
+        //money script..?
 //win detection
+//Need an option to watch advertisements to gain gold button
+//buy Fragments with moneyyyyy
+
+    public void OnVictory() {
+        //Save current health
+        //determine score increase from enemy type and time and chance
+        //chance to add currency
+        //chance for item reward
+    }
+
+    public void OnDefeat() {
+        //Reset health
+        //determine final score - highscore
+        //add total currency
+        //total scores
+    }
 }
