48350dbaee65bcc8110628d65ab615e9e38a7ffa
Soverilus <30487703+Soverilus@users.noreply.github.com>
2019-03-13 13:22:34 +0800

    fuck

---
+++ HackAndSlashProj/Assets/Scripts/GameLoopController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoopController : MonoBehaviour
+{
+//Load health between scenes - save current health as PlayerPref?
+//Score keeping - currency and gold prize for winning
+//win detection
+}
