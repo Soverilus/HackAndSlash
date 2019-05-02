ad0681a7a7903fbebd9791fc07c39e3a07288415
Soverilus <30487703+Soverilus@users.noreply.github.com>
2019-05-02 20:00:39 +0800

    all the stuff is ready

--- HackAndSlashProj/Assets/AndroidPauser.cs
+++ HackAndSlashProj/Assets/AndroidPauser.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 using UnityEngine.Android;
 public class AndroidPauser : MonoBehaviour
 {
+    bool isAndroid;
     public GameObject entireScene;
     private void Start() {
-        
+#if UNITY_ANDROID
+        isAndroid = true;
+#else
+        isAndroid = false;
+#endif
+
+        if (isAndroid) {
+            Destroy(gameObject);
+        }
     }
 
     private void OnApplicationFocus(bool focus) {
-        entireScene.SetActive(false);
+        if (isAndroid) {
+            entireScene.SetActive(false);
+        }
     }
 
     private void Update() {
-        for (int i = 0; i < Input.touches.Length; i++) {
-            TouchPhase myTP = Input.touches[i].phase;
-            if (myTP == TouchPhase.Began||
-                myTP == TouchPhase.Moved||
-                myTP == TouchPhase.Stationary){
-                entireScene.SetActive(true);
+        if (isAndroid) {
+            for (int i = 0; i < Input.touches.Length; i++) {
+                TouchPhase myTP = Input.touches[i].phase;
+                if (myTP == TouchPhase.Began ||
+                    myTP == TouchPhase.Moved ||
+                    myTP == TouchPhase.Stationary) {
+                    entireScene.SetActive(true);
+                }
             }
         }
     }
